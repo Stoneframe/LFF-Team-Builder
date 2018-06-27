6b3672d79c2e877f1064e8dc85db4cb32cb17c72
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2018-06-27 21:54:42 +0200

    Create TeamsValidator

---
+++ src/teamsbuilder/TeamsValidator.java
@@ -0,0 +1,45 @@
+package teamsbuilder;
+
+import java.util.List;
+import java.util.function.Function;
+
+import model.Team;
+
+public class TeamsValidator
+{
+	private Function<Integer, Boolean> scoringRule;
+
+	private int minimumNumberOfPlayers;
+	private int maximumNumberOfPlayers;
+
+	public TeamsValidator(
+			Function<Integer, Boolean> scoringRule,
+			int minimumNumberOfPlayers,
+			int maximumNumberOfPlayers)
+	{
+		this.minimumNumberOfPlayers = minimumNumberOfPlayers;
+		this.maximumNumberOfPlayers = maximumNumberOfPlayers;
+	}
+
+	public boolean areValid(List<Team> teams)
+	{
+		return allTeamsHaveScoreablePlayers(teams)
+				&& noTeamsHaveTooFewPlayers(teams)
+				&& noTeamsHaveTooManyPlayers(teams);
+	}
+
+	private boolean allTeamsHaveScoreablePlayers(List<Team> teams)
+	{
+		return teams.stream().allMatch(t -> t.numberOfScoreablePlayers(scoringRule) > 0);
+	}
+
+	private boolean noTeamsHaveTooFewPlayers(List<Team> teams)
+	{
+		return teams.stream().allMatch(t -> t.numberOfPlayers() > minimumNumberOfPlayers);
+	}
+
+	private boolean noTeamsHaveTooManyPlayers(List<Team> teams)
+	{
+		return teams.stream().allMatch(t -> t.numberOfPlayers() < maximumNumberOfPlayers);
+	}
+}
