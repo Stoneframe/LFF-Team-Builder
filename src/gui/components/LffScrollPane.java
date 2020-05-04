35d0b69c7e8fd6183ba059693398d650ef80284a
Paul Steneram Bibby <paul.steneram.bibby@gmail.com>
2020-05-05 00:40:20 +0200

    Change color of scoll bars

--- src/gui/components/LffScrollPane.java
+++ src/gui/components/LffScrollPane.java
@@ -3,8 +3,10 @@ package gui.components;
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.plaf.basic.BasicScrollBarUI;
 
 import gui.Util;
 
@@ -20,5 +22,39 @@ public class LffScrollPane
 		setBorder(BorderFactory.createLineBorder(Util.FOREGROUND, 2));
 		setPreferredSize(dimension);
 		setBackground(Util.BACKGROUND);
+
+		getVerticalScrollBar().setBackground(Util.BACKGROUND);
+		getHorizontalScrollBar().setBackground(Util.BACKGROUND);
+
+		getVerticalScrollBar().setUI(new BasicScrollBarUI()
+		{
+			@Override
+			protected void configureScrollBarColors()
+			{
+				this.thumbColor = Util.FOREGROUND;
+			}
+
+			@Override
+			protected JButton createDecreaseButton(int orientation)
+			{
+				return createZeroButton();
+			}
+
+			@Override
+			protected JButton createIncreaseButton(int orientation)
+			{
+				return createZeroButton();
+			}
+
+			private JButton createZeroButton()
+			{
+				JButton button = new JButton("zero button");
+				Dimension zeroDim = new Dimension(0, 0);
+				button.setPreferredSize(zeroDim);
+				button.setMinimumSize(zeroDim);
+				button.setMaximumSize(zeroDim);
+				return button;
+			}
+		});
 	}
 }
